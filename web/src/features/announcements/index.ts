091d63a833e0845b6705bdfda49174d9fdfe8977
mryeminaung <yeminaung013@gmail.com>
2026-07-24 14:32:58 +0630

    feat: redesign announcement page & add service layer for it

--- web/src/features/announcements/index.ts
+++ web/src/features/announcements/index.ts
@@ -1,4 +1,5 @@
 // features/announcements/index.ts
 
 export * from "./announcement.types";
+export * from "./services/announcement.service";
 export { default as AnnouncementsPage } from "./pages/announcements-page";
